80d6bf57c4819b4fb9d24c1992de74096f86a63c
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2021-01-09 18:52:22 +0100

    fixed

--- movies_library.py
+++ movies_library.py
@@ -1,45 +1,59 @@
-class Library():
+from random import randint
+
+class Movies():
     def __init__(self,title,release,genre,watched):
         self.title=title
         self.release=release
         self.genre=genre
         self.watched = watched
     def __str__(self):
-        return f"{self.title} was relased in {self.watched}"
-    def play(self):
-        return self.watched + 1
-movie1=Library("Monthy Python", 1982, "comedy",998)
-movie2=Library("Titanic", 1999, "thiller",129)
-movie3=Library("Park", 2001, "horror",4521)
-movie4=Library("Parasite", 2018, "thiller",3699)
-print(movie2)
-class Series():
-    def __init__(self, title, release, genre, episode, season, watched):
-        self.title=title
-        self.release=release
-        self.genre=genre
-        self.episode=episode
-        self.season=season
-        self.watched = watched
+        return f"{self.title} was relased in {self.release}"
+    def play(self, step=1):
+        self.watched += step
+    def current_views(self):
+        return self.watched
+    
+class Series(Movies):
+  def __init__(self,title,release,genre,episode,season,watched):
+    super(self.__class__,self).__init__(title,release,genre,watched)
+    self.episode=episode
+    self.season=season
     def __str__(self):
-        return f"{self.title} {self.episode}{self.season}"
-Se1 = Series("Dark",2017,"comedy","E01","S02",2789)
-Se2 = Series("Adnie",2013,"comedy","E01","S02",111)
-Se3 = Series("Friends",1995,"comedy","E04","S01",27569)
+        return f"{self.title}\n {self.episode}\n{self.season}\n"
+
 
 library=[
-    ("Movie 1"),("Monthy Python"), ("release",1982), ("genre","comedy"),("watched",998),
-    ("Movie 2"),("Titanic"), ("release",1999), ("genre","romantic thiller"),("watched",239),
-    ("Movie 3"),("Park Southern"), ("release",2014), ("thiller"),("watched",34),
-    ("Movie 4"),("Forrest Gump"), ("release",1994), ("comedy"),("watched",2978),
-    ("Serial 1"),("Dark"), ("release",2017), ("comedy"),("watched",998),("episode","E01"),("season","S02"),("watched",2789),
-    ("Serial 2"),("Friends"), ("release",1998), ("comedy"),("watched",998),("episode","E01"),("season","S02"),("watched",234),
-    ("Serial 3"),("Anie"), ("release",2017), ("comedy"),("watched",789),("episode","E02"),("season","S04"),("watched",564),
-    ("Serial 4"),("Queen"), ("release",2015), ("comedy"),("watched",12345),("episode","E01"),("season","S07"),("watched",349077),
-]
+    Movies(title="Monthy Python", release = 1982, genre="comedy", watched=998),
+    Movies(title="Titanic", release=1999, genre="thiller",watched=129),
+    Series(title="Dark",release=2017,genre="comedy",episode="E01",season="S02",watched=2789),
+    Series(title="Friends",release=1995,genre="comedy",episode="E04",season="S01",watched=27569)
+    ]
+
+def top_titles():
+    for a,b in enumerate(library):
+        if b.watched > 130:
+            print(f"{b} with {b.watched} views")
+top_titles()
+#def generate_views(times=10):
+    #generate_views()
+def search(library, title):
+    for picture in library:
+        if picture.title == title:
+            return picture
+
+def get_movies():
+    for row in library:
+        if isinstance (row,Movies):
+            print(f"{row.title}")
+get_movies()
+
+def get_series():
+    for row in library:
+        if isinstance (row,Series):
+            print(f"{row.title}")
+get_series()
 
 
-print(type(library))
-print(movie3.play())
-print(movie3)
-print(Se1)
+
+
+print(search(library,"Titanic"))
